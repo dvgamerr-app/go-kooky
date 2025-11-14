d16d9531cd2386faa325e9f27ecefd821c1afbbb
Kananek Thongkam <info.dvgamer@gmail.com>
2025-11-14 17:22:32 +0700

    feat: deprecated Internet Explorer, Konqueror, Lynx, Netscape, Uzbl, and W3M cookie stores

--- browser/all/import.go
+++ browser/all/import.go
@@ -1,20 +1,10 @@
 package all
 
 import (
-	_ "github.com/dvgamerr-app/go-kooky/browser/browsh"
 	_ "github.com/dvgamerr-app/go-kooky/browser/chrome"
 	_ "github.com/dvgamerr-app/go-kooky/browser/chromium"
-	_ "github.com/dvgamerr-app/go-kooky/browser/dillo"
 	_ "github.com/dvgamerr-app/go-kooky/browser/edge"
-	_ "github.com/dvgamerr-app/go-kooky/browser/elinks"
-	_ "github.com/dvgamerr-app/go-kooky/browser/epiphany"
 	_ "github.com/dvgamerr-app/go-kooky/browser/firefox"
-	_ "github.com/dvgamerr-app/go-kooky/browser/ie"
-	_ "github.com/dvgamerr-app/go-kooky/browser/konqueror"
-	_ "github.com/dvgamerr-app/go-kooky/browser/lynx"
-	_ "github.com/dvgamerr-app/go-kooky/browser/netscape"
 	_ "github.com/dvgamerr-app/go-kooky/browser/opera"
 	_ "github.com/dvgamerr-app/go-kooky/browser/safari"
-	_ "github.com/dvgamerr-app/go-kooky/browser/uzbl"
-	_ "github.com/dvgamerr-app/go-kooky/browser/w3m"
 )
